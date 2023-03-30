7e9591a7a9232534359f1ec2325098e48edfbdcd
developertariq <dev.tariqul.islam@gmail.com>
2023-03-30 12:22:36 +0600

    Add method

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,12 @@
 class Comment < ApplicationRecord
-  belongs_to :user
+  belongs_to :author, class_name: 'User'
   belongs_to :post
 
-  def update_likes
-    self.likes += 1
-    save
+  after_save :update_post_comments_counter
+
+  private
+
+  def update_post_comments_counter
+    post.update(comments_counter: post.comments.count)
   end
 end

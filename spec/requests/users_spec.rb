1db2e3325b18ace77ed6901543d26f59ef4d3458
developertariq <dev.tariqul.islam@gmail.com>
2023-04-05 13:08:34 +0600

    Create users_spec.rb

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,82 @@
+require 'rails_helper'
+
+RSpec.describe PostsController, type: :request do
+  let!(:user) { User.create(name: 'Tariq', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
+  let!(:post) { Post.create(author: user, title: 'My First Post', text: 'Lorem ipsum') }
+  describe 'GET #index' do
+    it 'returns http success' do
+      get user_posts_path(user_id: user.id)
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the index template' do
+      get user_posts_path(user_id: user.id)
+      expect(response).to render_template(:index)
+    end
+
+    it 'includes correct placeholder text in the response body' do
+      get user_posts_path(user_id: user.id)
+      expect(response.body).to include(user.name)
+      expect(response.body).to include(post.title)
+      expect(response.body).to include(post.text)
+    end
+  end
+
+  describe 'GET #show' do
+    it 'returns http success' do
+      get user_post_path(user_id: user.id, id: post.id)
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the show template' do
+      get user_post_path(user_id: user.id, id: post.id)
+      expect(response).to render_template(:show)
+    end
+
+    it 'includes correct placeholder text in the response body' do
+      get user_post_path(user_id: user.id, id: post.id)
+      expect(response.body).to include(post.title)
+      expect(response.body).to include(post.text)
+      expect(response.body).to include(user.name)
+    end
+  end
+end
+
+RSpec.describe UsersController, type: :request do
+  let!(:user) { User.create(name: 'Tariq', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
+  let!(:post) { Post.create(author: user, title: 'My First Post', text: 'Lorem ipsum') }
+  describe 'GET #index' do
+    it 'returns http success' do
+      get users_path
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the index template' do
+      get users_path
+      expect(response).to render_template(:index)
+    end
+
+    it 'includes correct placeholder text in the response body' do
+      get users_path
+      expect(response.body).to include(user.name)
+    end
+  end
+
+  describe 'GET #show' do
+    it 'returns http success' do
+      get user_path(user)
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the show template' do
+      get user_path(user)
+      expect(response).to render_template(:show)
+    end
+
+    it 'includes correct placeholder text in the response body' do
+      get user_path(user)
+      expect(response.body).to include(user.name)
+      expect(response.body).to include(post.title)
+    end
+  end
+end

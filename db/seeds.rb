c54ece3f7df99a572c654ab1eadf250f969a0cdb
developertariq <dev.tariqul.islam@gmail.com>
2023-04-06 15:27:41 +0600

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# # Users
+
+user1 = User.create(name: 'Tsohle', photo: 'https://picsum.photos/200/200', bio: 'Teacher from South Africa.')
+user2 = User.create(name: 'Teklay', photo: 'https://picsum.photos/300/300', bio: 'Teacher from Etheopia.')
+# # Posts 
+
+post1 = Post.create(author: user1, title: 'Hello', text: 'This is my first post.')
+post2 = Post.create(author: user1, title: 'Hello', text: 'This is my first post.')
+post3 = Post.create(author: user1, title: 'Hello', text: 'This is my first post.')
+post4 = Post.create(author: user1, title: 'Hello', text: 'This is my first post.')
+post5 = Post.create(author: user1, title: 'Hello', text: 'This is my first post.')
+post6 = Post.create(author: user1, title: 'Hello', text: 'This is my first post.')
+post7 = Post.create(author: user1, title: 'Hello', text: 'This is my first post.')
+
+# # Comments
+
+Comment.create(author: user1, post: post1, text: 'Sure. Great reminder, thanks!')
+Comment.create(author: user1, post: post1, text: 'So relevant. Keep it up!')
+Comment.create(author: user1, post: post1, text: 'Thank you so much!')
+Comment.create(author: user1, post: post1, text: 'So smart... Lorem ipsum sit dolor amit.')
+Comment.create(author: user1, post: post1, text: 'Sure. Great reminder, thanks!')
+Comment.create(author: user1, post: post1, text: 'So relevant. Keep it up!')
+Comment.create(author: user1, post: post1, text: 'Thank you so much!')
+Comment.create(author: user1, post: post1, text: 'So smart... Lorem ipsum sit dolor amit.')
+Comment.create(author: user1, post: post1, text: 'So smart... Lorem ipsum sit dolor amit.')
+Comment.create(author: user1, post: post1, text: 'So smart... Lorem ipsum sit dolor amit.')
+Comment.create(author: user1, post: post1, text: 'So smart... Lorem ipsum sit dolor amit.')
+Comment.create(author: user1, post: post1, text: 'So smart... Lorem ipsum sit dolor amit.')
+Comment.create(author: user1, post: post1, text: 'So smart... Lorem ipsum sit dolor amit.')
+Comment.create(author: user1, post: post1, text: 'So smart... Lorem ipsum sit dolor amit.')
+Comment.create(author: user1, post: post1, text: 'So smart... Lorem ipsum sit dolor amit.')
+Comment.create(author: user2, post: post1, text: 'So smart... Lorem ipsum sit dolor amit.')
+Comment.create(author: user1, post: post1, text: 'So smart... Lorem ipsum sit dolor amit.')

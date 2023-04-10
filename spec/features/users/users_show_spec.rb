68b69c42951b4beff0608e1b0934e148169cb398
developertariq <dev.tariqul.islam@gmail.com>
2023-04-10 22:39:54 +0600

    Fix linters

--- spec/features/users/users_show_spec.rb
+++ spec/features/users/users_show_spec.rb
@@ -8,8 +8,8 @@ RSpec.describe 'User Index Page', type: :feature do
       posts_counter: 4
     )
     @post1 = Post.create(author: @user, title: 'Hello', text: 'Hello World!')
-    @post2= Post.create(author: @user, title: 'Howdy', text: 'This is test post.')
-    @post3= Post.create(author: @user, title: 'Greetings', text: 'Good evening from Nigeria')
+    @post2 = Post.create(author: @user, title: 'Howdy', text: 'This is test post.')
+    @post3 = Post.create(author: @user, title: 'Greetings', text: 'Good evening from Nigeria')
     visit user_path(@user)
   end
 
@@ -36,15 +36,15 @@ RSpec.describe 'User Index Page', type: :feature do
   scenario 'shows user\'s first three posts' do
     visit user_path(@user)
     posts = @user.recent_posts.limit(3)
-      posts.each do |post|
-        expect(page).to have_content(post.title)
-        expect(page).to have_content(post.text)
-      end
+    posts.each do |post|
+      expect(page).to have_content(post.title)
+      expect(page).to have_content(post.text)
+    end
   end
 
   scenario 'shows button that lets user view all of a user\'s posts.' do
     user_path(@user)
-    expect(page).to have_link("See all posts", href: user_posts_path(@user))
+    expect(page).to have_link('See all posts', href: user_posts_path(@user))
   end
 
   scenario 'it redirects me to that post\'s show page.' do
@@ -55,7 +55,7 @@ RSpec.describe 'User Index Page', type: :feature do
 
   scenario 'it redirects me to the user\'s post\'s index page.' do
     visit user_path(@user)
-    click_link("See all posts")
+    click_link('See all posts')
     expect(page).to have_current_path(user_posts_path(@user))
   end
 end

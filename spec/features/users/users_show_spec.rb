2504f9c1c4fbe3139d3cfb42b31ab95d91ebb4b8
developertariq <dev.tariqul.islam@gmail.com>
2023-04-10 21:54:52 +0600

    Refactor test cases for users show file
    - Update users data according to seed.rb file
    - Add three post for this user

--- spec/features/users/users_show_spec.rb
+++ spec/features/users/users_show_spec.rb
@@ -1,55 +1,61 @@
 require 'rails_helper'
-Rspec.describe 'User Index Page', type: :feature do
+RSpec.describe 'User Index Page', type: :feature do
   before :each do
     @user = User.create(
-      name: 'Tariq Islam'
-      photo: 'https://images.pexels.com/photos/4050426/pexels-photo-4050426.jpeg'
-      bio: 'chill with laptop'
-      posts_counter: 2
+      name: 'Ellon',
+      photo: 'https://picsum.photos/300/200',
+      bio: 'Software Engineer from Nigeria',
+      posts_counter: 4
     )
-    visit users_path
+    @post1 = Post.create(author: @user, title: 'Hello', text: 'Hello World!')
+    @post2= Post.create(author: @user, title: 'Howdy', text: 'This is test post.')
+    @post3= Post.create(author: @user, title: 'Greetings', text: 'Good evening from Nigeria')
+    visit user_path(@user)
   end
 
   scenario 'shows the profile picture for each user' do
-    visit '/users'
+    user_path(@user)
     expect(page).to have_css('img')
   end
 
   scenario 'shows the user\'s username' do
-    visit '/users'
+    user_path(@user)
     expect(page).to have_content(@user.name)
   end
 
   scenario 'shows number of posts user has written' do
-    visit '/users'
+    user_path(@user)
     expect(page).to have_content(@user.posts_counter)
   end
 
   scenario 'shows user\'s bio' do
-    visit '/users'
+    user_path(@user)
     expect(page).to have_content(@user.bio)
   end
 
   scenario 'shows user\'s first three posts' do
-    visit '/users'
-    expect(page).to have_content(@user.posts_counter.order(created_at: :desc).limit(3))
+    visit user_path(@user)
+    posts = @user.recent_posts.limit(3)
+      posts.each do |post|
+        expect(page).to have_content(post.title)
+        expect(page).to have_content(post.text)
+      end
   end
 
   scenario 'shows button that lets user view all of a user\'s posts.' do
-    visit '/users'
-    click_button('See all post')
-    expect(page).to have_button('See all post')
+    user_path(@user)
+    expect(page).to have_link("See all posts", href: user_posts_path(@user))
   end
 
   scenario 'it redirects me to that post\'s show page.' do
-    visit '/users'
-    click_button('See all post')
-    expect(page).to have_content('This is my first post')
+    visit user_path(@user)
+    click_link('Hello')
+    expect(page).to have_current_path(user_post_path(@user, @post1))
   end
 
   scenario 'it redirects me to the user\'s post\'s index page.' do
-    visit '/users'
-    click_button('See post')
-    expect(page).to have_button('User Post')
+    visit user_path(@user)
+    click_link("See all posts")
+    expect(page).to have_current_path(user_posts_path(@user))
   end
 end

96edec18a91cdb2f6239e98fcb57b5bb7ffb1f3d
developertariq <dev.tariqul.islam@gmail.com>
2023-04-12 09:45:53 +0600

    Refactor for sign up

--- Gemfile
+++ Gemfile
@@ -57,6 +57,7 @@ group :development, :test do
   gem 'factory_bot_rails'
   gem 'rspec-rails'
   gem 'devise'
+  gem 'letter_opener'
 end
 
 group :development do
--- db/schema.rb
+++ db/schema.rb
@@ -58,23 +58,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_11_144401) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
-    t.integer "failed_attempts", default: 0, null: false
-    t.string "unlock_token"
-    t.datetime "locked_at"
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["photo"], name: "index_users_on_photo"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
   add_foreign_key "comments", "posts"

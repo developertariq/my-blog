0eda29929051b4d5c61fc49719543152081184bf
developertariq <dev.tariqul.islam@gmail.com>
2023-04-13 14:46:23 +0600

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "users#index"
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :new, :create, :show] do
-      resources :comments, only: [:create]
+    resources :posts, only: [:index, :new, :create, :show, :destroy] do
+      resources :comments, only: [:create, :destroy]
       resources :likes, only: [:create]
     end
   end 
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
 end

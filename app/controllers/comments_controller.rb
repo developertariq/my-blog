8f3b1b84d07ffc3a8f07c2dd5134189f3a336d71
developertariq <dev.tariqul.islam@gmail.com>
2023-04-13 14:49:20 +0600

    Refactor code

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,4 +7,17 @@ class CommentsController < ApplicationController
       render :new
     end
   end
+
+  # def destroy
+  #   @comment = Comment.find(params[:id])
+  #   authorize! :destroy, @comment 
+
+  #   if @comment.destroy
+  #     flash[:success] = "Comment deleted successfully."
+  #     redirect_to user_post_path(@comment.post.author, @comment.post)
+  #   else
+  #     flash[:error] = "Failed to delete comment."
+  #     redirect_to @comment.post
+  #   end
+  # end
 end

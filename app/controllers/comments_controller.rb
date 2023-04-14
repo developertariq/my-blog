2d55c0f1e1ac1410a1995e05ce99421dee89bf6d
developertariq <dev.tariqul.islam@gmail.com>
2023-04-15 00:51:07 +0600

    Fix all errors

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,20 +1,20 @@
 class CommentsController < ApplicationController
   def index
-    user = User.find(params[:user_id])
-    post = user.posts.find(params[:post_id])
-    comments = post.comments
+    @user = User.find(params[:user_id])
+    @post = @user.posts.find(params[:post_id])
+    @comments = @post.comments
     respond_to do |format|
-      format.json { render json: comments }
+      format.html { redirect_to user_post_path(params[:user_id], params[:post_id]) }
+      format.json { render json: @comments.as_json(only: %i[id text author_id post_id]) }
     end
   end
 
   def create
     @comment = Comment.new(author_id: params[:user_id], post_id: params[:post_id], text: params[:comment][:text])
-    redirect_to user_post_path(params[:user_id], params[:post_id]), notice: 'Comment was successfully created.'
     respond_to do |format|
       if @comment.save
         format.html do
-          redirect_to user_post_path(params[:user_id], params[:post_id]), notice: 'Comment was successfully created.'
+          redirect_to user_post_path(@comment.author, @comment.post), notice: 'Comment was successfully created.'
         end
         format.json do
           render json: { status: 'success', message: 'Comment created successfully', data: @comment }, status: :created
--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,8 @@ Rails.application.routes.draw do
   root "users#index"
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :new, :create, :show, :destroy] do
-      resources :comments, only: [:create, :destroy]
+      resources :comments, only: [:index, :create, :destroy]
       resources :likes, only: [:create]
     end
   end 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # Defines the root path route ("/")
-  # root "articles#index"
 end

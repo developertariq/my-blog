bc8f4843809023d516376f5a0934b9e9543e58f3
Newton <altontonnalumasa@gmail.com>
2023-04-15 03:04:36 +0300

    Configure swagger_helper file

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -20,49 +20,49 @@ RSpec.configure do |config|
   # document below. You can override this behavior by adding a swagger_doc tag to the
   # the root example_group in your specs, e.g. describe '...', swagger_doc: 'v2/swagger.json'
 
-  config.swagger_docs = {
-    'v2/swagger.yaml' => {
-      openapi: '3.0.1',
-      info: {
-        title: 'API V1',
-        version: 'v1'
-      },
-      paths: {},
-      servers: [
-        {
-          url: 'https://{defaultHost}',
-          variables: {
-            defaultHost: {
-              default: 'www.example.com'
-            }
-          }
-        }
-      ]
-    }
-  }
-
   # config.swagger_docs = {
-  #   'v1/swagger.json' => {
+  #   'v2/swagger.yaml' => {
   #     openapi: '3.0.1',
-  #     components: {
-  #       securitySchemes: {
-  #         basic_auth: {
-  #           type: :http,
-  #           scheme: :basic
-  #         },
-  #         api_key: {
-  #           type: :apiKey,
-  #           name: 'api_key',
-  #           in: :query
+  #     info: {
+  #       title: 'API V1',
+  #       version: 'v1'
+  #     },
+  #     paths: {},
+  #     servers: [
+  #       {
+  #         url: 'https://{defaultHost}',
+  #         variables: {
+  #           defaultHost: {
+  #             default: 'www.example.com'
+  #           }
   #         }
   #       }
-  #     }
+  #     ]
   #   }
   # }
+
+  config.swagger_docs = {
+    'v1/swagger.json' => {
+      openapi: '3.0.1',
+      components: {
+        securitySchemes: {
+          basic_auth: {
+            type: :http,
+            scheme: :basic
+          },
+          api_key: {
+            type: :apiKey,
+            name: 'api_key',
+            in: :query
+          }
+        }
+      }
+    }
+  }
   # Specify the format of the output Swagger file when running 'rswag:specs:swaggerize'.
   # The swagger_docs configuration option has the filename including format in
   # the key, this may want to be changed to avoid putting yaml in json files.
   # Defaults to json. Accepts ':json' and ':yaml'.
-  # config.swagger_format = :json
+  config.swagger_format = :json
   config.swagger_format = :yaml
 end
